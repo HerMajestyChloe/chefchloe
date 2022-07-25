54f3cf8487a2b456c479bc114129b819eb246834
HerMajestyChloe <109038623+HerMajestyChloe@users.noreply.github.com>
2022-07-25 11:52:28 -0400

    Add files via upload

---
+++ docs/javascript/searchlist.js
@@ -0,0 +1,33 @@
+function search_posts() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('posts');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
+
+
+
+
+
+function search_categories() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('categories');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
